e260e6490ce86d70b10b2f563644dc8898d8fdae
João Cardoso <joaofaccardoso@gmail.com>
2020-11-22 21:22:27 +0000

    Final commit

--- Homomorphic_enc.py
+++ Homomorphic_enc.py
@@ -5,26 +5,15 @@ import time
 
 pd.options.display.float_format = "{:,.4f}".format
 
-gun_violence_path = 'Datasets/gun_violence/gun-violence-data_01-2013_03-2018.csv'
+gun_violence_path = 'gun-violence-data_01-2013_03-2018.csv'
 
 impt_columns = [
     'incident_id',
     'date',
     'state',
-    'city_or_county',
     'n_killed',
     'n_injured'
 ]
-
-n_samples_test = [
-    1000,
-    2000,
-    4000,
-    8000,
-    16000,
-    32000
-]
-
 
 def read_file():
     return pd.read_csv(gun_violence_path)
@@ -44,8 +33,9 @@ def encrypt_dataset(HE, df):
 
     init_time = time.time()
     
-    df['n_killed'] = list(map(lambda x : HE.encryptFrac(x),df['n_killed']))
-    df['n_injured'] = list(map(lambda x : HE.encryptFrac(x),df['n_injured']))
+    for index, rows in df.iterrows():
+        df.loc[index ,'n_killed'] = HE.encryptFrac(df.loc[index ,'n_killed'])
+        df.loc[index, 'n_injured'] = HE.encryptFrac(df.loc[index ,'n_injured'])
 
     final_time = time.time() - init_time
 
@@ -81,27 +71,48 @@ def homomorphic_ops(HE, df):
 
 if __name__ == "__main__":
     df = read_file()
-    df = df.loc[df['date'].str.contains('2017'), impt_columns].copy()
+    df = df.loc[(df['date'].str.contains('2017')) | (df['date'].str.contains('2016')), impt_columns].copy()
 
-    n_samples_test.append(len(df))
+    n_samples_test = []
+    i = len(df)
+    while i > 20:
+        n_samples_test.append(i)
+        i = round(i/2)
+
     operation_times = []
 
     HE = create_pyfhel_obj()
-    [df_test, encrypt_time] = encrypt_dataset(HE, df)
+    [df_enc, encrypt_time] = encrypt_dataset(HE, df)
+    print('Encryption time: {:.4f} seconds'.format(encrypt_time))
 
     for num in n_samples_test:
         print('Number of Samples: {:d}'.format(num))
+        df_test = df_enc[:num].copy()
+        states = df_test['state'].unique().tolist()
         [stats, ops_time] = homomorphic_ops(HE, df_test)
-        print('Done, next please!\n')
 
-        operation_times.append(operation_times)
+        frame = {
+            'state': states,
+            'n_incidents':[HE.decryptInt(stats[i][0]) for i in range(len(stats))],
+            'n_killed':[HE.decryptFrac(stats[i][1]) for i in range(len(stats))],
+            'killed_mean':[HE.decryptFrac(stats[i][2]) for i in range(len(stats))],
+            'n_injured':[HE.decryptFrac(stats[i][3]) for i in range(len(stats))],
+            'injured_mean':[HE.decryptFrac(stats[i][4]) for i in range(len(stats))]
+        }
+
+        result_df = pd.DataFrame(frame)
+        result_df.to_csv('df'+str(num)+'.csv')
+
+        print('Done! Next please!\n')
+
+        operation_times.append(ops_time)
+
+    print('All done!')
 
     frame = {
         'n_samples': n_samples_test,
-        'operations_time': operation_times
+        'operation_times': operation_times
     }
 
     times_df = pd.DataFrame(frame)
-    print(encrypt_time)
-    print(n_samples_test)
-    print(operation_times)
+    times_df.to_csv('times_df.csv')
